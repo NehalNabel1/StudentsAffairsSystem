05cb2cc695b25d46f3946a688360908fbd8b6c7f
NehalNabel1 <nehalnabel6464@gmail.com>
2026-02-12 22:06:15 +0200

    create add edit classes

---
+++ src/modules/add/AddStudents.js
@@ -0,0 +1,27 @@
+import { CRUD } from "../CRUD/CRUD.js";
+export class AddStudents extends CRUD {
+
+  add = async (newStudent) => {
+    try {
+      /*get all student to assign unique id */
+      let allRes = await fetch("http://localhost:3000/students");
+      let allStudents = await allRes.json();
+      let ids = allStudents.map((student) => student.id);
+      let maxId = allStudents.length ? Math.max(...ids) : 0;
+      newStudent.id = maxId +1;
+
+      let res = await fetch("http://localhost:3000/students", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newStudent),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to add student");
+      }
+      let students = await res.json();
+      return students;
+    } catch (err) {
+      console.log("Error : ", err);
+    }
+  };
+}
--- src/modules/view/ViewPeople.js
+++ src/modules/view/ViewPeople.js
@@ -2,7 +2,7 @@ import { CRUD } from "../CRUD/CRUD.js";
 export class ViewCourses extends CRUD {
   view = async () => {
     try {
-      let res = await fetch("http://localhost:3000/courses");
+      let res = await fetch("http://localhost:3000/people");
       let students = await res.json();
       return students;
     } catch (err) {
